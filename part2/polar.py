d2d5f889b26f6b30b7d46e62eec52bf6a3b8e7d9
mehtasau <mehtasau@iu.edu>
2021-12-01 01:24:27 -0500

    added readme, changed transition probability and emission probability functions, added screenshots for bayes, hmm and human inputs

--- part2/polar.py
+++ part2/polar.py
@@ -13,7 +13,6 @@ import sys
 import imageio
 from scipy.stats import norm
 import numpy as np
-import matplotlib.pyplot as plt
 
 # calculate "Edge strength map" of an image                                                                                                                                      
 def edge_strength(input_image):
@@ -22,10 +21,18 @@ def edge_strength(input_image):
     filters.sobel(grayscale,0,filtered_y)
     return sqrt(filtered_y**2)
 
+# Calculate emission probability
+# This function takes image and edge strength as input 
+# and returns emission probability for that image
+# @param: image: Image array
+# @param: edge_strength: Gives us a derviative of image. Higher the value
+# in edge strength, more likely a point is to be a part of a boundary
+# We take the ratio of edge strength to image intensity to calculate emission probabilities
 def get_emission(image, edge_strength):
     nrows = edge_strength.shape[0]
     ncols = edge_strength.shape[1]
     emission = np.zeros((nrows, ncols))
+    
     for r in range(nrows):
         for c in range(ncols):
             emission[r][c] = edge_strength[r][c]/image[r][c]
@@ -41,16 +48,16 @@ def get_emission(image, edge_strength):
 # @param: row2: Row of next column
 """
         Some other distance distributions tried:
-        1.) diff.append((len - dist)/len)
-        2.) diff.append(1/(dist+1))
+        1.) len - dist)/len
+        2.) 1/(dist+1)
 """
-def get_distance_distr1(row1, row2):
+def get_transition_prob(row1, row2):
      
     dist = abs(row1-row2)
     if dist <=6:
         return -np.log(1/((dist+1)))
     else:
-        return -np.log((2/(2**(dist+1)))**2) 
+        return -np.log((1/(2**(dist+1)))**2) 
         
 # draw a "line" on an image (actually just plot the given y-coordinates
 #  for each x-coordinate)
@@ -93,10 +100,13 @@ def write_output_image(filename, image, simple, hmm, feedback, feedback_pt):
 
 # generate air-ice and ice-rock boundary using simple bayes
 # This function takes edge strength map as input and  
-# returns list of rows containining boundary points for air-ice and ice-rock
+# returns list of rows containining boundary points for air-ice and 
+# a list containing boundary points for ice-rock
 # @param: edge_strength: Gives us a derviative of image. Higher the value
 # in edge strength, more likely a point is to be a part of a boundary.
-# We calculate the 
+# We calculate the air ice boundary by taking maximum value in a colum
+# We calculate the ice-rock boundary by taking maximum value 10px below 
+# air ice boundary
 def get_bayes_rows(edge_strength):
     max_rows_list = []
     next_rows_list = []
@@ -112,20 +122,27 @@ def get_bayes_rows(edge_strength):
     next_rows_list = [row + median_air_ice_row+10 for row in next_rows_list]
     return (max_rows_list, next_rows_list)
 
-# 
+# generate air-ice and ice-rock boundary using hidden markov model(Viterbi)
+# This function takes edge strength map and image array as input and  
+# returns list of rows containining boundary points for air-ice and 
+# a list containing boundary points for ice-rock
+# @param: edge_strength: Gives us a derviative of image. Higher the value
+# in edge strength, more likely a point is to be a part of a boundary
+# @param: image: Image array
+# using viterbi algorithm, we find the air-ice boundary points 
+# and then we get ice-rock boundary by performing viterbi for rows 
+# 10 px below air-ice row. 
+# We consider minimum cost instead of maximum probability to avoid underflow issues
+
 def get_hmm_rows(image,edge_strength):
     edge_strength = edge_strength+1
     ncols = edge_strength.shape[1]
     nrows = edge_strength.shape[0]
+    min_state = np.zeros((nrows, ncols))
 
     V_table = np.ones((nrows, ncols))
     emission_probs = -np.log(get_emission(image, edge_strength))
     V_table[:,0] = -np.log(edge_strength[:,0]/np.sum(edge_strength[:,0]))+ emission_probs[:,0]
-
-    # # calculation for viterbi table 
-    # as minimum cost function
-
-    min_state = np.zeros((nrows, ncols))
 
     # Calculation for air ice boundary
     for t in range(1, edge_strength.shape[1]):
@@ -133,7 +150,7 @@ def get_hmm_rows(image,edge_strength):
             min_cost = np.Infinity
             for r1 in range(edge_strength.shape[0]):
 
-                distance_distr = get_distance_distr1(row, r1)
+                distance_distr = get_transition_prob(row, r1)
                 transition = distance_distr
                 val = V_table[r1][t-1]+transition 
                 if(val < min_cost):
@@ -141,18 +158,17 @@ def get_hmm_rows(image,edge_strength):
                     min_state[row][t] = r1
                     V_table[row][t] = min_cost + emission_probs[row][t]
 
-    mini = np.argmin(V_table[:,-1])
+    bkptr = np.argmin(V_table[:,-1])
     ridge_air_ice = np.zeros(ncols)
     for col in range(ncols-1, -1, -1):
-        ridge_air_ice[col] = int(mini)
-        mini = min_state[int(mini)][col]
+        ridge_air_ice[col] = int(bkptr)
+        bkptr = min_state[int(bkptr)][col]
 
     # # Calculation for ice-rock boundary
 
     V_table_ir = np.ones((nrows, ncols))*np.Infinity
     min_state = np.zeros((nrows, ncols))
-    median_air_ice_row = int(median(ridge_air_ice))
-    for row in range(median_air_ice_row+10, edge_strength.shape[0]):
+    for row in range(int(ridge_air_ice[0])+10, edge_strength.shape[0]):
         V_table_ir[row,0] = -np.log(edge_strength[row][0]/np.sum(edge_strength[:,0])) + emission_probs[row,0]
 
     for col in range(ncols):
@@ -163,7 +179,7 @@ def get_hmm_rows(image,edge_strength):
                 min_cost = np.Infinity
                 for r1 in range(edge_strength.shape[0]):
 
-                    distance_distr = get_distance_distr1(row, r1)
+                    distance_distr = get_transition_prob(row, r1)
                     #transition = distance_distr
                     transition = distance_distr
                     val = V_table_ir[r1][t-1]+transition 
@@ -172,20 +188,36 @@ def get_hmm_rows(image,edge_strength):
                         min_state[row][t] = r1
                         V_table_ir[row][t] = min_cost + emission_probs[row][t]
                     
-    mini = np.argmin(V_table_ir[:,-1])
+    bkptr = np.argmin(V_table_ir[:,-1])
     ridge_ice_rock = np.zeros(ncols)
     for col in range(ncols-1, -1, -1):
-        ridge_ice_rock[col] = int(mini)
-        mini = min_state[int(mini)][col]
+        ridge_ice_rock[col] = int(bkptr)
+        bkptr = min_state[int(bkptr)][col]
     return (ridge_air_ice,ridge_ice_rock)
 
+# generate air-ice boundary using hidden markov model(Viterbi) and 
+# human feedback
+# This function takes edge strength map and image array as input and  
+# returns list of rows containining boundary points for air-ice and 
+# a list containing boundary points for ice-rock
+# @param: edge_strength: Gives us a derviative of image. Higher the value
+# in edge strength, more likely a point is to be a part of a boundary
+# @param: image: Image array
+# @param: row_coord: Row number input by human for air ice boundary
+# @param: col_coord: Column number input by human for air ice boundary
+# using viterbi algorithm, we find the air-ice boundary points.
+# We run viterbi in two directions, 1.) from col_coord till all we reach the 
+# end of columns. 2.) From col_coord to the start of columns.
+# We consider minimum cost instead of maximum probability to avoid underflow issues
 
-def get_feedback_rows_air_ice(edge_strength, row_coord, col_coord):
+def get_feedback_rows_air_ice(edge_strength, image,row_coord, col_coord):
     edge_strength = edge_strength+1
     nrows = edge_strength.shape[0]
     ncols = edge_strength.shape[1]
+    
     emission_probs = edge_strength/sum(edge_strength, axis = 0)
     emission_probs = -np.log(emission_probs)
+    #emission_probs = -np.log(get_emission(image, edge_strength))
 
     #create viterbi table
     V_table = np.ones((edge_strength.shape[0], edge_strength.shape[1]))
@@ -198,7 +230,7 @@ def get_feedback_rows_air_ice(edge_strength, row_coord, col_coord):
             min_cost = np.Infinity
             for r1 in range(edge_strength.shape[0]):
 
-                distance_distr = get_distance_distr1(row, r1)
+                distance_distr = get_transition_prob(row, r1)
                 transition = distance_distr
                 val = V_table[r1][t+1]+transition 
                 if(val < min_cost):
@@ -211,7 +243,7 @@ def get_feedback_rows_air_ice(edge_strength, row_coord, col_coord):
             min_cost = np.Infinity
             for r1 in range(edge_strength.shape[0]):
 
-                distance_distr = get_distance_distr1(row, r1)
+                distance_distr = get_transition_prob(row, r1)
                 transition = distance_distr
                 val = V_table[r1][t-1]+transition 
                 if(val < min_cost):
@@ -219,47 +251,54 @@ def get_feedback_rows_air_ice(edge_strength, row_coord, col_coord):
                     min_state[row][t] = r1
                     V_table[row][t] = min_cost + emission_probs[row][t]
             
-    mini = np.argmin(V_table[:,-1])
+    backptr = np.argmin(V_table[:,-1])
     ridge = np.zeros(ncols)
     for col in range(ncols-1, -1, -1):
-        ridge[col] = int(mini)
-        mini = min_state[int(mini)][col]    
+        ridge[col] = int(backptr)
+        backptr = min_state[int(backptr)][col]    
 
     return ridge
 
-def get_feedback_rows_ice_rock(edge_strength, row_coord, col_coord, air_ice):
+# generate ice-rock boundary using hidden markov model(Viterbi) and 
+# human feedback
+# This function takes edge strength map and image array as input and  
+# returns list of rows containining boundary points for ice-rock 
+# @param: edge_strength: Gives us a derviative of image. Higher the value
+# in edge strength, more likely a point is to be a part of a boundary
+# @param: image: Image array
+# @param: row_coord: Row number input by human for ice-rock boundary
+# @param: col_coord: Column number input by human for ice-rock boundary
+# @param: air_ice: list of rows for air-ice boundary
+# using viterbi algorithm, we find the ice-rock boundary points. We consider rows 
+# below 10px from the air-ice boundary points
+# We run viterbi in two directions, 1.) from col_coord till all we reach the 
+# end of columns. 2.) From col_coord to the start of columns.
+# We consider minimum cost instead of maximum probability to avoid underflow issues
+def get_feedback_rows_ice_rock(edge_strength,image, row_coord, col_coord, air_ice):
     edge_strength = edge_strength+1
     nrows = edge_strength.shape[0]
     ncols = edge_strength.shape[1]
     emission_probs = edge_strength/sum(edge_strength, axis = 0)
     emission_probs = -np.log(emission_probs)
+    #emission_probs = -np.log(get_emission(image, edge_strength))
 
     #create viterbi table
     V_table = np.ones((edge_strength.shape[0], edge_strength.shape[1]))*np.Infinity
+    for row in range(int(air_ice[0])+10, edge_strength.shape[0]):
+        V_table[row,0] = -np.log(edge_strength[row][0]/np.sum(edge_strength[:,0])) + emission_probs[row,0]
+
     V_table[:,col_coord] = np.Infinity
     V_table[row_coord,col_coord] = -np.log(1)
     min_state = np.zeros((nrows, ncols))
             
-    for t in range(col_coord-1, 0,-1):
-        for row in range(nrows):
-            min_cost = np.Infinity
-            for r1 in range(int(air_ice[t])+10, nrows):
-
-                distance_distr = get_distance_distr1(row, r1)
-                #transition = distance_distr
-                transition = distance_distr
-                val = V_table[r1][t+1]+transition 
-                if(val < min_cost):
-                    min_cost = val
-                    min_state[row][t] = r1
-                    V_table[row][t] = min_cost + emission_probs[row][t]
+    
     
     for t in range(col_coord+1, edge_strength.shape[1]):
         for row in range(int(air_ice[t])+10, edge_strength.shape[0]):
             min_cost = np.Infinity
             for r1 in range(edge_strength.shape[0]):
 
-                distance_distr = get_distance_distr1(row, r1)
+                distance_distr = get_transition_prob(row, r1)
                 transition = distance_distr
                 val = V_table[r1][t-1]+transition 
                 if(val < min_cost):
@@ -267,12 +306,23 @@ def get_feedback_rows_ice_rock(edge_strength, row_coord, col_coord, air_ice):
                     min_state[row][t] = r1
                     V_table[row][t] = min_cost + emission_probs[row][t]
 
+    for t in range(col_coord-1, 0,-1):
+        for row in range(nrows):
+            min_cost = np.Infinity
+            for r1 in range(int(air_ice[t])+10, nrows):
 
-    mini = np.argmin(V_table[:,-1])
+                distance_distr = get_transition_prob(row, r1)
+                transition = distance_distr
+                val = V_table[r1][t+1]+transition 
+                if(val < min_cost):
+                    min_cost = val
+                    min_state[row][t] = r1
+                    V_table[row][t] = min_cost + emission_probs[row][t]
+    backptr = np.argmin(V_table[:,-1])
     ridge = np.zeros(ncols)
     for col in range(ncols-1, -1, -1):
-        ridge[col] = int(mini)
-        mini = min_state[int(mini)][col]
+        ridge[col] = int(backptr)
+        backptr = min_state[int(backptr)][col]
     return ridge
 
 # main program
@@ -300,12 +350,12 @@ if __name__ == "__main__":
 
     airice_simple = simple_bayes[0]
     airice_hmm = hmm_bayes[0]
-    airice_feedback= get_feedback_rows_air_ice(edge_strength,gt_airice[1], gt_airice[0] )
+    airice_feedback= get_feedback_rows_air_ice(edge_strength,image_array,gt_airice[1], gt_airice[0] )
 
 
     icerock_simple = simple_bayes[1]
     icerock_hmm = hmm_bayes[1]
-    icerock_feedback= get_feedback_rows_ice_rock(edge_strength, gt_icerock[1],gt_icerock[0],airice_feedback)
+    icerock_feedback= get_feedback_rows_ice_rock(edge_strength,image_array, gt_icerock[1],gt_icerock[0],airice_feedback)
 
     # Now write out the results as images and a text file
     write_output_image("air_ice_output.png", input_image, airice_simple, airice_hmm, airice_feedback, gt_airice)
